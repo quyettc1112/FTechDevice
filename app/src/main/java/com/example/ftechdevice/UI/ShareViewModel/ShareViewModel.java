32dc3903616e5c859976d093e20d8b69be07bbf8
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-14 13:10:22 +0700

    Update Setup Project

---
+++ app/src/main/java/com/example/ftechdevice/UI/ShareViewModel/ShareViewModel.java
@@ -0,0 +1,79 @@
+package com.example.ftechdevice.UI.ShareViewModel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.ftechdevice.Model.CartModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
+public class ShareViewModel extends ViewModel {
+
+    private final MutableLiveData<List<CartModel>> _cartItems = new MutableLiveData<>(new ArrayList<>());
+    public LiveData<List<CartModel>> getCartItems() {
+        return _cartItems;
+    }
+
+    @Inject
+    public ShareViewModel() {
+    }
+
+    public void addItem(CartModel cartModel) {
+        List<CartModel> currentList = _cartItems.getValue();
+        if (currentList == null) {
+            currentList = new ArrayList<>();
+        }
+
+        CartModel existingItem = null;
+        for (CartModel item : currentList) {
+            if (item.getToyModel().getId() == cartModel.getToyModel().getId()) {
+                existingItem = item;
+                break;
+            }
+        }
+
+        if (existingItem != null) {
+            existingItem.setQuantity(existingItem.getQuantity() + 1);
+        } else {
+            currentList.add(cartModel);
+        }
+
+        _cartItems.setValue(currentList);
+    }
+
+    public void removeItem(CartModel cartModel) {
+        List<CartModel> currentList = _cartItems.getValue();
+        if (currentList == null) {
+            currentList = new ArrayList<>();
+        }
+
+        CartModel existingItem = null;
+        for (CartModel item : currentList) {
+            if (item.getId() == cartModel.getId()) {
+                existingItem = item;
+                break;
+            }
+        }
+
+        if (existingItem != null) {
+            if (existingItem.getQuantity() > 1) {
+                existingItem.setQuantity(existingItem.getQuantity() - 1);
+            } else {
+                currentList.remove(existingItem);
+            }
+        }
+
+        _cartItems.setValue(currentList);
+    }
+
+    public void updateCartItems(List<CartModel> newCartItems) {
+        _cartItems.setValue(newCartItems);
+    }
+}
