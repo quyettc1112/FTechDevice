f8a1f7927b47a2490f7f1154287b26254196e296
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-14 13:07:03 +0700

    Update Setup Project

---
+++ app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_ftechdevice_AppConfig_Application_FTechDevice.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.ftechdevice.AppConfig.Application.FTechDevice",
+    rootPackage = "com.example.ftechdevice.AppConfig.Application",
+    originatingRoot = "com.example.ftechdevice.AppConfig.Application.FTechDevice",
+    originatingRootPackage = "com.example.ftechdevice.AppConfig.Application",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "FTechDevice",
+    originatingRootSimpleNames = "FTechDevice"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_example_ftechdevice_AppConfig_Application_FTechDevice {
+}
