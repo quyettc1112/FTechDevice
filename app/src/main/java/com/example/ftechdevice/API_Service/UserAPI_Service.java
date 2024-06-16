6f59e92dedf701402b2c6b5ada65e0fbe124e72f
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-17 00:20:54 +0700

    Update Authen

---
+++ app/src/main/java/com/example/ftechdevice/API_Service/UserAPI_Service.java
@@ -0,0 +1,19 @@
+package com.example.ftechdevice.API_Service;
+
+import com.example.ftechdevice.Model.ModelRequestDTO.JWTObject;
+import com.example.ftechdevice.Model.ModelRequestDTO.RegisterRequestDTO;
+import com.example.ftechdevice.Model.ModelRequestDTO.UserCretidentialDTO;
+import com.example.ftechdevice.Model.ModelRespone.MessageRespone;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UserAPI_Service {
+
+    @POST("/auth/sign-in")
+    Call<JWTObject> getUserCretidential(@Body UserCretidentialDTO userCretidentialDTO);
+
+    @POST("/auth/sign-up")
+    Call<MessageRespone> callRegisterUser(@Body RegisterRequestDTO registerRequestDTO);
+}
---
+++ app/src/main/java/com/example/ftechdevice/UI/ShareViewModel/UserShareViewModel.java
@@ -0,0 +1,43 @@
+package com.example.ftechdevice.UI.ShareViewModel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.ftechdevice.Model.ModelRequestDTO.JWTObject;
+import com.example.ftechdevice.Model.ModelRequestDTO.UserCretidentialDTO;
+
+public class UserShareViewModel extends ViewModel {
+    private final MutableLiveData<UserCretidentialDTO> _userCredentials = new MutableLiveData<>();
+    public LiveData<UserCretidentialDTO> getUserCredentials() {
+        return _userCredentials;
+    }
+
+    private final MutableLiveData<JWTObject> _jwtToken = new MutableLiveData<>();
+    public LiveData<JWTObject> getJwtToken() {
+        return _jwtToken;
+    }
+
+    // User Credentials
+    public void updateUserCretidential(UserCretidentialDTO userCretidentialDTO) {
+        _userCredentials.setValue(userCretidentialDTO);
+    }
+
+    public UserCretidentialDTO getUserCretidentail() {
+        return _userCredentials.getValue();
+    }
+
+    // User JWT
+    public void updateJWTToken(JWTObject jwtObject) {
+        _jwtToken.setValue(jwtObject);
+    }
+
+    public JWTObject getJWTToken() {
+        return _jwtToken.getValue();
+    }
+
+    // Function to clear JWT Token
+    public void clearJWTToken() {
+        _jwtToken.setValue(null);
+    }
+}
