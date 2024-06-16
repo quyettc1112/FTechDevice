c8af58b542c821bb9fab4f7345eb958d608cf3d3
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-17 00:20:48 +0700

    Update Authen

---
+++ app/src/main/java/com/example/ftechdevice/API_Repository/UserAPI_Repository.java
@@ -0,0 +1,31 @@
+package com.example.ftechdevice.API_Repository;
+
+import com.example.ftechdevice.API_Service.UserAPI_Service;
+import com.example.ftechdevice.Model.ModelRequestDTO.JWTObject;
+import com.example.ftechdevice.Model.ModelRequestDTO.RegisterRequestDTO;
+import com.example.ftechdevice.Model.ModelRequestDTO.UserCretidentialDTO;
+import com.example.ftechdevice.Model.ModelRespone.MessageRespone;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.scopes.ActivityScoped;
+import retrofit2.Call;
+
+@ActivityScoped
+public class UserAPI_Repository {
+
+    private final UserAPI_Service userapiService;
+
+    @Inject
+    public UserAPI_Repository(UserAPI_Service userapiService) {
+        this.userapiService = userapiService;
+    }
+
+    public Call<JWTObject> getUserCretidential(UserCretidentialDTO userCretidentialDTO) {
+        return userapiService.getUserCretidential(userCretidentialDTO);
+    }
+
+    public Call<MessageRespone> callRegister(RegisterRequestDTO registerRequestDTO) {
+        return userapiService.callRegisterUser(registerRequestDTO);
+    }
+}
