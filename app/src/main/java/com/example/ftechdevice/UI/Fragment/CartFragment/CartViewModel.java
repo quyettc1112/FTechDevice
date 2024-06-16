f8e6b67dd6716d4062142dea549c25ec7dbc268a
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-16 23:42:43 +0700

    Update UI Settup Project

---
+++ app/src/main/java/com/example/ftechdevice/UI/Fragment/CartFragment/CartViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ftechdevice.UI.Fragment.CartFragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class CartViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
