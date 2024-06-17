fe5f8fbd9e3dabe91eed3b24b5efaba2c7d61828
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-17 16:16:55 +0700

    Update Maps Location

--- app/src/main/java/com/example/ftechdevice/UI/Activity/MapActivity/MapsActivity.java
+++ app/src/main/java/com/example/ftechdevice/UI/Activity/MapActivity/MapsActivity.java
@@ -2,9 +2,15 @@ package com.example.ftechdevice.UI.Activity.MapActivity;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.ftechdevice.AppConfig.CustomView.CustomDialog.ConfirmDialog;
+import com.example.ftechdevice.AppConfig.CustomView.CustomToolBar.CustomToolbar;
+import com.example.ftechdevice.Common.Constants.Constants;
 import com.example.ftechdevice.R;
+import com.example.ftechdevice.UI.Activity.MainActivity.MainActivity;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,41 +18,180 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.ftechdevice.databinding.ActivityMapsBinding;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+    private AutocompleteSupportFragment autocompleteSupportFragment;
+    private MapsViewModels mapsViewModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mapsViewModels = new ViewModelProvider(this).get(MapsViewModels.class);
+
+        Places.initialize(getApplicationContext(), "AIzaSyA-Mz1r06CJkEJQKLENv3AlaGC9BXUZ1Os");
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        binding.customToolbar.setOnStartIconClickListener(new CustomToolbar.OnStartIconClickListener() {
+            @Override
+            public void onStartIconClick() {
+                Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                intent.putExtra(Constants.IS_BACK_FROM_MAP, true);
+                startActivity(intent);
+            }
+        });
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Thu Duc, Ho Chi Minh City and move the camera
-        LatLng thuDuc = new LatLng(10.8491, 106.7715); // Tọa độ của Thủ Đức
-        mMap.addMarker(new MarkerOptions().position(thuDuc).title("Marker in Thu Duc, Ho Chi Minh City"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(thuDuc));
+        if (isLocationPermissionGranted()) {
+            showCurrentLocation();
+            findPlace();
+        } else {
+            requestLocationPermission();
+        }
     }
+
+    private void findPlace() {
+        autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.autoComplete_fragment);
+        autocompleteSupportFragment.setPlaceFields(List.of(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onError(@NonNull Status status) {
+                //Toast.makeText(MapsActivity.this, status.getStatusMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), DEFAULT_ZOOM));
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName()));
+            }
+        });
+    }
+
+
+    private boolean isLocationPermissionGranted() {
+        return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showCurrentLocation();
+            } else {
+                Intent intent = new Intent(this, MainActivity.class);
+                Intent intentToSetting = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                ConfirmDialog confirmDialog = new ConfirmDialog(
+                        this,
+                        new ConfirmDialog.ConfirmCallback() {
+                            @Override
+                            public void negativeAction() {
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void positiveAction() {
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intentToSetting.setData(uri);
+                                startActivity(intentToSetting);
+                            }
+                        },
+                        "Cannot get location",
+                        "Grant permission to access location",
+                        "Yes",
+                        "No"
+                );
+                confirmDialog.show();
+            }
+        }
+    }
+
+    private void showCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            );
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current Location"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM));
+            }
+        });
+    }
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
 }
