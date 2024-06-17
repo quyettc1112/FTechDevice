e499cbe608c5a652701b9ae65a7284f4e50bbd18
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-17 12:32:06 +0700

    Update cart UI

--- app/src/main/java/com/example/ftechdevice/UI/Fragment/CartFragment/CartFragment.java
+++ app/src/main/java/com/example/ftechdevice/UI/Fragment/CartFragment/CartFragment.java
@@ -14,25 +14,117 @@ import android.view.ViewGroup;
 
 import com.example.ftechdevice.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.ftechdevice.UI.ShareViewModel.ShareViewModel;
+import com.example.ftechdevice.databinding.FragmentCartBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+
 public class CartFragment extends Fragment {
 
-    private CartViewModel mViewModel;
+    private FragmentCartBinding binding;
+    private CartAdapter cartAdapter;
+    private ShareViewModel sharedViewModel;
 
-    public static CartFragment newInstance() {
-        return new CartFragment();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        cartAdapter = new CartAdapter();
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(ShareViewModel.class);
+        cartAdapter.submitList(sharedViewModel.getCartItems().getValue());
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCartBinding.inflate(inflater, container, false);
+
+        binding.rlCart.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.rlCart.setAdapter(cartAdapter);
+
+        checkShowUI();
+        setAddOrRemoveQuantity();
+        observeViewModel();
+        showPaymentDialog();
+        return binding.getRoot();
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(CartViewModel.class);
-        // TODO: Use the ViewModel
+    private void setAddOrRemoveQuantity() {
+        cartAdapter.setOnAddQuantityItemClickListener(item -> {
+            Toast.makeText(requireContext(), "Add", Toast.LENGTH_SHORT).show();
+            cartAdapter.addItem(item.getToyModel());
+            return null;
+        });
+
+        cartAdapter.setOnRemoveQuantityItemClickListener(item -> {
+            Toast.makeText(requireContext(), "Minus", Toast.LENGTH_SHORT).show();
+            sharedViewModel.removeItem(item);
+            checkShowUI();
+            return null;
+        });
     }
 
+    private void observeViewModel() {
+        sharedViewModel.getCartItems().observe(getViewLifecycleOwner(), cartItems -> {
+            cartAdapter.updateCartItems(cartItems);
+            checkShowUI();
+        });
+    }
+
+    private void showPaymentDialog() {
+        binding.btnPayment.setOnClickListener(v -> {
+            BottomSheetDialog dialog = new BottomSheetDialog(requireContext());
+            View view = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_proceed_to_payment, null);
+            view.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    convertDpToPx(430)
+            ));
+
+            TextView tong_tien_hang = view.findViewById(R.id.tv_tong_tien_hang);
+            tong_tien_hang.setText(formatPrice(cartAdapter.getTotalItemsPrice()) + " VND");
+
+            TextView tong_tien = view.findViewById(R.id.tv_tong_tien_thanh_toan);
+            tong_tien.setText(formatPrice(cartAdapter.getTotalItemsPrice() + 20000.0) + " VND");
+
+          /*  view.findViewById(R.id.btn_payment).setOnClickListener(v1 -> startActivity(new Intent(requireContext(), PaymentActivity.class)));*/
+
+            dialog.setContentView(view);
+            dialog.show();
+        });
+    }
+
+    private int convertDpToPx(int dp) {
+        float density = requireContext().getResources().getDisplayMetrics().density;
+        return (int) (dp * density);
+    }
+
+    private void checkShowUI() {
+        if (cartAdapter.getCurrentList().size() > 0) {
+            binding.layoutEmptyCart.setVisibility(View.GONE);
+            binding.layoutCart.setVisibility(View.VISIBLE);
+        } else {
+            binding.layoutEmptyCart.setVisibility(View.VISIBLE);
+            binding.layoutCart.setVisibility(View.GONE);
+        }
+    }
+
+    private String formatPrice(double price) {
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        return formatter.format(price);
+    }
 }
