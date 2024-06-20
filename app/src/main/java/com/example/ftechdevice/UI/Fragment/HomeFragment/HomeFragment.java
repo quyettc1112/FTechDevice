7ed2e8afb09f95c3c28422b74fb9f072ce418e4f
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-20 15:24:36 +0700

    Update product UI

--- app/src/main/java/com/example/ftechdevice/UI/Fragment/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/example/ftechdevice/UI/Fragment/HomeFragment/HomeFragment.java
@@ -20,23 +20,43 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.ftechdevice.API_Repository.ProductAPI_Repository;
+import com.example.ftechdevice.API_Repository.UserAPI_Repository;
 import com.example.ftechdevice.Common.CommonAdapter.CategoryOptionAdapter;
 import com.example.ftechdevice.Common.CommonAdapter.CategoryOptionInteraction;
+import com.example.ftechdevice.Common.CommonAdapter.ProductListAdapter;
 import com.example.ftechdevice.Common.CommonAdapter.ToyListAdapter;
 import com.example.ftechdevice.Common.CommonAdapter.VideoMainAdapter;
 import com.example.ftechdevice.Common.Constants.Constants;
+import com.example.ftechdevice.Model.ModelRespone.ProductReponse;
+import com.example.ftechdevice.Model.ProductModel;
+import com.example.ftechdevice.Model.ToyModel;
 import com.example.ftechdevice.R;
 import com.example.ftechdevice.UI.Activity.VideoActivity.VideoActivity;
 import com.example.ftechdevice.Until.BottomMarginItemDecoration;
 import com.example.ftechdevice.Until.NonScrollableGridLayoutManager;
 import com.example.ftechdevice.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+@AndroidEntryPoint
 public class HomeFragment extends Fragment implements CategoryOptionInteraction, CategoryOptionAdapter.CategoryOptionInteraction {
 
     private FragmentHomeBinding binding;
     private VideoMainAdapter videoAdapter;
     private CategoryOptionAdapter cateOptionAdapter;
     private ToyListAdapter toyListAdapter;
+    private ProductListAdapter productListAdapter;
+    @Inject
+    ProductAPI_Repository productAPIRepository;
+    private List<ProductModel> productList;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,7 +64,8 @@ public class HomeFragment extends Fragment implements CategoryOptionInteraction,
         videoAdapter = new VideoMainAdapter(Constants.getListCourse());
         cateOptionAdapter = new CategoryOptionAdapter(Constants.getListString(), this);
         toyListAdapter = new ToyListAdapter(Constants.getListToys());
-
+        productListAdapter = new ProductListAdapter(new ArrayList<>());
+        callProductAPI();
 
         toyListAdapter.setOnItemCartClickListener(cartModel -> {
             Toast.makeText(requireContext(), cartModel.getToyName(), Toast.LENGTH_SHORT).show();
@@ -59,23 +80,22 @@ public class HomeFragment extends Fragment implements CategoryOptionInteraction,
         setIndicator();
         setCurrentIndicator(0);
         setRecycleCateOption();
-        setRecycleToysList();
+        setRecycleProductList();
         intentToVideoActivity();
         return binding.getRoot();
-
     }
 
     private void setRecycleCateOption() {
         binding.myRecyclerView.setAdapter(cateOptionAdapter);
     }
 
-    private void setRecycleToysList() {
+    private void setRecycleProductList() {
         binding.rvToys.setLayoutManager(new NonScrollableGridLayoutManager(requireContext(), 2));
-        binding.rvToys.setAdapter(toyListAdapter);
+        binding.rvToys.setAdapter(productListAdapter);
 
         int bottomMarginInPx = convertDpToPx(20);
         binding.rvToys.addItemDecoration(new BottomMarginItemDecoration(bottomMarginInPx));
-        int itemCount = toyListAdapter.getItemCount();
+        int itemCount = productListAdapter.getItemCount();
         int rowCount = (itemCount % 2 == 0) ? itemCount / 2 : (itemCount / 2) + 1;
         int newHeight = rowCount * convertDpToPx(300) + (rowCount - 1) * bottomMarginInPx;
 
@@ -148,4 +168,27 @@ public class HomeFragment extends Fragment implements CategoryOptionInteraction,
             startActivity(new Intent(requireActivity(), VideoActivity.class));
         });
     }
+
+    private void callProductAPI(){
+        productAPIRepository.getProductList("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJlbWFpbCI6ImFkbWluMUBnbWFpbC5jb20iLCJ1c2VySWQiOjEsIlJvbGVOYW1lIjoiQURNSU4iLCJpYXQiOjE3MTg4MTQyNjUsImV4cCI6MTcxODkwMDY2NX0.RSZfNylizNft4eXkXVUc1Zci5FFIUCak9wUuFq3E0zI",
+                0, 12, "", 0).enqueue(new Callback<ProductReponse>() {
+            @Override
+            public void onResponse(Call<ProductReponse> call, Response<ProductReponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<ProductModel> products = response.body().getContent();
+                    Log.d("Check value", "Products size: " + (products != null ? products.size() : 0));
+                    if (products != null) {
+                        productListAdapter.setProductList(products);
+                    }
+                } else {
+                    Log.d("Check value", "Response code: " + response.code());
+                }
+            }
+            @Override
+            public void onFailure(Call<ProductReponse> call, Throwable t) {
+                Log.d("Check value", t.getMessage());
+            }
+        });
+    }
+
 }
