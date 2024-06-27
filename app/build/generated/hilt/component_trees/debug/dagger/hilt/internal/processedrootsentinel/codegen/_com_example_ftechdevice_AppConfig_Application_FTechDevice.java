1850dab1c5fe74036a15cbea404ea1291a359aae
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-27 21:10:10 +0700

    Update Setup

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_ftechdevice_AppConfig_Application_FTechDevice.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.ftechdevice.AppConfig.Application.FTechDevice"
-)
-public final class _com_example_ftechdevice_AppConfig_Application_FTechDevice {
-}
