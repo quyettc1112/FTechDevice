c7e2c3fde6ebd415e0d40c8e0853687798233205
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-06-28 14:00:42 +0700

    Update Setup

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/ftechdevice/databinding/FragmentHomeBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/ftechdevice/databinding/FragmentHomeBinding.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -33,6 +34,9 @@ public final class FragmentHomeBinding implements ViewBinding {
 
   @NonNull
   public final LinearLayout horizontalLayout;
+
+  @NonNull
+  public final ImageView imNotify;
 
   @NonNull
   public final LinearLayout indicatorContainer;
@@ -82,11 +86,12 @@ public final class FragmentHomeBinding implements ViewBinding {
   private FragmentHomeBinding(@NonNull ConstraintLayout rootView,
       @NonNull ConstraintLayout constraintLayout, @NonNull ConstraintLayout constraintLayout3,
       @NonNull EditText edtSearch, @NonNull LinearLayout horizontalLayout,
-      @NonNull LinearLayout indicatorContainer, @NonNull LinearLayout llBanphim,
-      @NonNull LinearLayout llDienthoai, @NonNull LinearLayout llLaptop,
-      @NonNull LinearLayout llManhinh, @NonNull RecyclerView myRecyclerView,
-      @NonNull RecyclerView rvToys, @NonNull RecyclerView rvVideo, @NonNull TextView textView,
-      @NonNull TextView textView3, @NonNull TextView textView4, @NonNull TextView textView5,
+      @NonNull ImageView imNotify, @NonNull LinearLayout indicatorContainer,
+      @NonNull LinearLayout llBanphim, @NonNull LinearLayout llDienthoai,
+      @NonNull LinearLayout llLaptop, @NonNull LinearLayout llManhinh,
+      @NonNull RecyclerView myRecyclerView, @NonNull RecyclerView rvToys,
+      @NonNull RecyclerView rvVideo, @NonNull TextView textView, @NonNull TextView textView3,
+      @NonNull TextView textView4, @NonNull TextView textView5,
       @NonNull TextView textViewTimeFlashSale, @NonNull TextView tvSeeAllCategory,
       @NonNull TextView tvSeeAllVideo) {
     this.rootView = rootView;
@@ -94,6 +99,7 @@ public final class FragmentHomeBinding implements ViewBinding {
     this.constraintLayout3 = constraintLayout3;
     this.edtSearch = edtSearch;
     this.horizontalLayout = horizontalLayout;
+    this.imNotify = imNotify;
     this.indicatorContainer = indicatorContainer;
     this.llBanphim = llBanphim;
     this.llDienthoai = llDienthoai;
@@ -159,6 +165,12 @@ public final class FragmentHomeBinding implements ViewBinding {
       id = R.id.horizontal_layout;
       LinearLayout horizontalLayout = ViewBindings.findChildViewById(rootView, id);
       if (horizontalLayout == null) {
+        break missingId;
+      }
+
+      id = R.id.im_notify;
+      ImageView imNotify = ViewBindings.findChildViewById(rootView, id);
+      if (imNotify == null) {
         break missingId;
       }
 
@@ -253,7 +265,7 @@ public final class FragmentHomeBinding implements ViewBinding {
       }
 
       return new FragmentHomeBinding((ConstraintLayout) rootView, constraintLayout,
-          constraintLayout3, edtSearch, horizontalLayout, indicatorContainer, llBanphim,
+          constraintLayout3, edtSearch, horizontalLayout, imNotify, indicatorContainer, llBanphim,
           llDienthoai, llLaptop, llManhinh, myRecyclerView, rvToys, rvVideo, textView, textView3,
           textView4, textView5, textViewTimeFlashSale, tvSeeAllCategory, tvSeeAllVideo);
     }
