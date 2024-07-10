1a7c140bbe53514519f101c793ac5e950e3b2ad4
RoesAncient <ngodinhlong25032001@gmail.com>
2024-07-10 16:49:10 +0700

    Fix bugs cart , update logic

--- app/src/main/java/com/example/ftechdevice/Common/CommonAdapter/OrderDetailAdapter.java
+++ app/src/main/java/com/example/ftechdevice/Common/CommonAdapter/OrderDetailAdapter.java
@@ -36,7 +36,6 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
     public void onBindViewHolder(@NonNull OrderDetailViewHolder holder, int position) {
         OrderDetailModel orderDetail = orderDetailList.get(position);
         holder.tvProductName.setText(orderDetail.getProduct().getName());
-        holder.tvProductDescription.setText(orderDetail.getProduct().getDescription());
         holder.tvProductQuantity.setText("Số lượng: " + orderDetail.getQuantity());
         holder.tvProductPrice.setText("Giá: " + orderDetail.getPrice() + " VND");
 
@@ -51,7 +50,6 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
     public static class OrderDetailViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvProductName;
-        TextView tvProductDescription;
         TextView tvProductQuantity;
         TextView tvProductPrice;
         ImageView ivProductImage;
@@ -59,7 +57,6 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
         public OrderDetailViewHolder(@NonNull View itemView) {
             super(itemView);
             tvProductName = itemView.findViewById(R.id.tv_product_name);
-            tvProductDescription = itemView.findViewById(R.id.tv_product_description);
             tvProductQuantity = itemView.findViewById(R.id.tv_product_quantity);
             tvProductPrice = itemView.findViewById(R.id.tv_product_price);
             ivProductImage = itemView.findViewById(R.id.iv_product_image);
